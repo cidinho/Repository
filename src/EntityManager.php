b70b134d23a9a80387a1790587898ffdd716cae9
Alcides Bezerra <cidinho.cide@gmail.com>
2017-07-17 14:02:20 -0300

    Configurando DAO

--- src/EntityManager.php
+++ src/EntityManager.php
@@ -3,15 +3,44 @@
 namespace EntitiesPHP\Repository;
 
 use Doctrine\ORM\ {
-    EntityManager,
+    EntityManager as Em,
+    Tools\SchemaTool,
     Tools\Setup
 };
 
 class EntityManager {
 
     /**
+     * Caminho para as entidades do modelo de persistência
+     */
+    private static $entidades = ['vendor\ablima\solicitacao'];
+
+    /**
+     * configurações de conexão. Coloque aqui os seus dados
+     */
+    private static $dbParams = ['driver' => 'mysqli',
+        'user' => 'root',
+        'password' => '',
+        'dbname' => 'solicitacao',
+        'charset' => 'utf8',
+        'host' => 'localhost'
+    ];
+
+    /**
+     * Modo de Desenvolvimento
+     * Padrão: FALSE
+     * Caso seja FALSE ele não permite o modo de criação ou alteração do 
+     * banco de dados, por esse motivo a constante deve ser alterada para FALSE
+     * quando o desenvolvimento estiver concluído e assim evitar a modificação
+     * indevida.
+     * 
+     * @var Boolean 
+     */
+    private static $isDevMod = \TRUE;
+
+    /**
      * Entity Manager
-     * @var EntityManager 
+     * @var EntityManager
      */
     private static $_entityManager = null;
 
@@ -23,25 +52,70 @@ class EntityManager {
     }
 
     /**
+     * setEntitiesPath
+     * 
+     * @param array $entidades
+     * @return \self
+     */
+    public static function setEntitiesPath(array $entidades): self {
+        self::$entidades;
+        return self;
+    }
+
+    /**
+     * setDbParams
+     * 
+     * @param array $dbParams
+     * @return \self
+     */
+    public static function setDbParams(array $dbParams): self {
+        self::$dbParams = $dbParams;
+        return self;
+    }
+
+    /**
      * Retorna \Doctrine\ORM\EntityManager
      * 
-     * @return EntityManager
+     * @return Em
      */
     public static function get_instance() {
 
         if (self::$_entityManager === null) {
             //setando as configurações definidas anteriormente
-            $config = Setup::createAnnotationMetadataConfiguration(self::$entidades, self::IS_DEV_MOD);
+            $config = Setup::createAnnotationMetadataConfiguration(self::$entidades, self::$isDevMod);
             $config->addCustomStringFunction('group_concat', 'Oro\ORM\Query\AST\Functions\String\GroupConcat');
             $config->addCustomNumericFunction('hour', 'Oro\ORM\Query\AST\Functions\SimpleFunction');
             $config->addCustomNumericFunction('timestampdiff', 'Oro\ORM\Query\AST\Functions\Numeric\TimestampDiff');
             $config->addCustomDatetimeFunction('date', 'Oro\ORM\Query\AST\Functions\SimpleFunction');
-            $config->setProxyDir($_SERVER['DOCUMENT_ROOT'] . '/sys/core/Doctrine/Proxies');
             //criando o Entity Manager com base nas configurações de dev e banco de dados
-            self::$_entityManager = EntityManager::create(self::$dbParams, $config);
+            self::$_entityManager = Em::create(self::$dbParams, $config);
         }
 
         return self::$_entityManager;
+    }
+
+    public static function updateSchema() {
+
+        $em = self::get_instance();
+        $tool = new SchemaTool($em);
+        $entidades = self::getAllEntities();
+        $classes = array();
+        foreach ($entidades as $entidade) {
+            $classes [] = $em->getClassMetadata($entidade);
+        }
+//        $tmpConnection = $em->getConnection();
+//        $tmpConnection->getSchemaManager()->createDatabase('solicitacao');
+        $tool->updateSchema($classes, 'force');
+    }
+
+    public static function getAllEntities() {
+        $entities = array();
+        $em = self::get_instance();
+        $meta = $em->getMetadataFactory()->getAllMetadata();
+        foreach ($meta as $m) {
+            $entities[] = $m->getName();
+        }
+        return $entities;
     }
 
 }
--- src/RepositoryAtomic.php
+++ src/RepositoryAtomic.php
@@ -2,6 +2,25 @@
 
 namespace EntitiesPHP\Repository;
 
-function save($entity):void {
-    
+use Symfony\Component\VarDumper\VarDumper;
+
+function save($entity): void {
+    $em = EntityManager::get_instance();
+    $em->persist($entity);
+    $em->flush();
+}
+
+function get(string $entity, int $id = 0) {
+    $em = EntityManager::get_instance();
+
+    if ($id !== 0) {
+        $retorno = $em->getRepository($entity)->find($id);
+    } else {
+        $retorno = $em->getRepository($entity)->findAll();
+    }
+    return $retorno;
+}
+
+function dump($var){
+    VarDumper::dump($var);
 }
